ff061b77581d0a7fee4caa169ea5e3110fa9cbe4
Sunny&Rainbow2912 <rainbow18831@gmail.com>
2025-01-16 09:38:45 -0800

    0.Initializr

---
+++ demo/src/test/java/cicd_springboot/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cicd_springboot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
